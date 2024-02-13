d7ff5ba37b5bbb5990e66f179dfd98aca3f8a596
Sourav Suvarna <souravsuvarna711@gmail.com>
2024-02-13 22:08:45 +0530

    Added Subsets.cpp

---
+++ Recursion/Subsets.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsets(int start, int n,vector<int>v,vector<int>&res)
+{
+    for(auto it:res) cout<<it<<" ";
+    cout<<endl;
+
+    for(int i=start; i<n; i++)
+    {
+        if(i >start && v[i] == v[i-1]) continue;
+        res.push_back(v[i]);
+        subsets(i+1,n,v,res);
+        res.pop_back();
+    }
+
+}
+int main()
+{
+    vector<int> v = {1,2,2};
+    int size = v.size();
+    vector<int>res = {};
+    subsets(0,size,v,res);
+    return 0;
+}
